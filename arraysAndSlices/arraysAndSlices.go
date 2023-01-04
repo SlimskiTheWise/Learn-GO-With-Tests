4b07728dfb73859edc85c6787a158169346c8b55
Seunghun Lee <hun2456@hanmail.net>
2023-01-04 22:56:11 +0900

    arrays and slices
    1. more practice of the TDD workflow
    2. How they have a fixed capacity but you can create new slices from old ones using append

---
+++ arraysAndSlices/arraysAndSlices.go
@@ -0,0 +1,30 @@
+package arraysAndSlices
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, value := range numbers {
+		sum += value
+	}
+	return sum
+}
+
+func SumAll(numberToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numberToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
